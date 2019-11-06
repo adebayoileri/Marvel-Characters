e860090ea932db8715c1e1450b32e18966270ac8
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-11-06 13:07:25 +0100

    refactor image directories

--- src/card.js
+++ src/card.js
@@ -1,16 +1,13 @@
 import React from "react";
 
 const Card = props => {
-  const { name, detail, src } = props;
+  const { name, detail, img } = props;
   return (
     <div className="dib br3 pa3 ma2 grow bw2 shadow5">
-      <img
-        src={`https://terrigen-cdn-dev.marvel.com/content/prod/1x/021slq_ons_crd_02.jpg`}
-        alt="hero_image"
-      />
-      <div className="bg-dark-red card-text">
-        <h2>{name}</h2>
-        <p>{detail}</p>
+      <img src={img} alt="hero_image" />
+      <div className="bg-black hover-bg-dark-red bg-animate card-text">
+        <h2 className="tc f2 white">{name}</h2>
+        <p className="tc white">{detail}</p>
       </div>
     </div>
   );
--- src/characters.js
+++ src/characters.js
@@ -2,13 +2,15 @@ export const character = [
   {
     id: 1,
     name: "Captain America",
-    src: "003cap_ons_crd_03.jpg",
+    src:
+      "https://terrigen-cdn-dev.marvel.com/content/prod/1x/021slq_ons_crd_02.jpg",
     detail: "The Leader Of All"
   },
   {
     id: 2,
     name: "Star Lord",
-    src: "021slq_ons_crd_02.jpg",
+    src:
+      "https://terrigen-cdn-dev.marvel.com/content/prod/1x/021slq_ons_crd_02.jpg",
     detail: "Lord Of The Galaxies"
   }
 ];
