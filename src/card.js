f2a9aa143a6842ac74d1e2479880c7ff23d753d2
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-11-06 12:41:39 +0100

    render data to react app

--- src/card.js
+++ src/card.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-const Card = () => {
+const Card = props => {
+  const { name, detail, src } = props;
   return (
     <div className="dib br3 pa3 ma2 grow bw2 shadow5">
       <img
-        src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/021slq_ons_crd_02.jpg"
+        src={`https://terrigen-cdn-dev.marvel.com/content/prod/1x/021slq_ons_crd_02.jpg`}
         alt="hero_image"
-        style={{}}
       />
       <div className="bg-dark-red card-text">
-        <h2>Star Lord</h2>
-        <p>Lord Of The Galaxy</p>
+        <h2>{name}</h2>
+        <p>{detail}</p>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "tachyons";
 import Card from "./card";
-
+import { character } from "./characters";
 import "./styles.css";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Card />
+        <Card
+          id={character[0].id}
+          name={character[0].name}
+          detail={character[0].detail}
+          img={character[0].src}
+        />
+        <Card
+          id={character[1].id}
+          name={character[1].name}
+          detail={character[1].detail}
+          img={character[1].src}
+        />
       </React.Fragment>
     );
   }
