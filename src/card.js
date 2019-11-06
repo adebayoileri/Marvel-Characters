41add86cc6297f5a27ad7ca56ac41b1c0b30b443
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-11-06 12:08:25 +0100

    created card components

---
+++ src/card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="dib br3 pa3 ma2 grow bw2 shadow5">
+      <img
+        src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/021slq_ons_crd_02.jpg"
+        alt="hero_image"
+        style={{}}
+      />
+      <div className="bg-dark-red card-text">
+        <h2>Star Lord</h2>
+        <p>Lord Of The Galaxy</p>
+      </div>
+    </div>
+  );
+};
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import "tachyons";
+import Card from "./card";
 
 import "./styles.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
-      </div>
+      <React.Fragment>
+        <Card />
+      </React.Fragment>
     );
   }
 }
