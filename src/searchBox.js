560355a2d2aa9b021e85bdb8f44b8992dfeec199
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-12-26 14:38:09 +0100

    ft(search-box):create search compnent

---
+++ src/searchBox.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "tachyons";
+import "./styles.css";
+
+class Search extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <input type="text" />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Search;
